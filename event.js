b7a17457344787204ec9e644613300db36f6ac27
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-05-19 11:45:29 +0530

    Add introduction to event and event handling in JS.

---
+++ event.js
@@ -0,0 +1,18 @@
+/* 
+In JavaScript, an EVENT is any action or occurrence that happens in the browser, such as a button click, mouse movement, key press, form submission, or page loading. These actions are detected by the browser and can be used to trigger specific functionality in a webpage.
+
+EVENT HANDLING: is the process of responding to these events using JavaScript. It allows developers to attach functions, called EVENT HANDLERS or EVENT LISTENERS, to HTML elements so that a particular action is performed whenever an event occurs.
+
+Learning events and event handling is important because they make web pages INTERACTIVE AND DYNAMIC. Without events, websites would remain static and unable to react to user actions. Features like button clicks, dropdown menus, form validation, image sliders, dark mode toggles, and real-time updates all depend on event handling. It forms the foundation of modern frontend web development and is one of the most essential concepts in JavaScript.
+
+*/
+
+//BROWSER MEIN PAGE PAR KOI BHI HARKAT KARO EVENT RAISE HO JAAEGA.
+//KUCH SCREEN PAR HO AUR AAPKO REACTION DENA HO TO US WAQT AAPKO EVENT HANDLE KARNA AANA CHAHIYE!
+//EVENT MATLAB HOTA HAI KOI ACTION HUA(eg: click)
+//EVENT LISTENER KA MATLAB HAI AAPNE KOI ACTION KA REACTION DIYA.(eg:click hone par screen black se white ho gya)
+
+let h1 = document.querySelector("h1");
+h1.addEventListener("click",function(){
+    h1.style.color = "Red";
+})
