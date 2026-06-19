96cd5ce2b039c80b0ed87948daf8c22ea0177fdd
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-06-19 12:56:42 +0530

    Add project_01 which will find users

---
+++ project_01.js
@@ -0,0 +1,41 @@
+const users = [
+  {
+    name: "Amisha Rathore",
+    pic: "https://i.pinimg.com/736x/43/89/ad/4389ad7f3d3be19ed3efb1abc32385e1.jpg",
+    bio: "pretty face, savage mind 💋✨ | not here to impress anyone"
+  },
+  {
+    name: "Kiara Malhotra",
+    pic: "https://i.pinimg.com/736x/e7/83/6d/e7836da9ae4115b8098496ce1d743875.jpg",
+    bio: "too glam to give a damn 💅🔥"
+  },
+  {
+    name: "Ayesha Kapoor",
+    pic: "https://i.pinimg.com/736x/e4/ae/21/e4ae2177891b051a5c76eba788f9e01c.jpg",
+    bio: "main character energy 🎀👑 | stay mad, i'm thriving"
+  },
+  {
+    name: "Riya Sethi",
+    pic: "https://i.pinimg.com/736x/75/55/06/755506f6147cbaffd7ba59d33476c3ec.jpg",
+    bio: "soft heart, sharp tongue 🖤🥀"
+  },
+  {
+    name: "Ananya Mehra",
+    pic: "https://i.pinimg.com/736x/ba/9f/e8/ba9fe8f87ead2a7260de8e50d7afcd53.jpg",
+    bio: "born to stand out, never to fit in ✨😌"
+  },
+  {
+    name: "Tanya Arora",
+    pic: "https://i.pinimg.com/736x/70/01/5d/70015d367bf637437379d5db34b37203.jpg",
+    bio: "expensive taste, free advice 💸😉"
+  },
+  {
+    name: "Myra Khanna",
+    pic: "https://i.pinimg.com/736x/70/d9/b1/70d9b1e2b3e101fe42a2d62b062b9d65.jpg",
+    bio: "collecting memories, not opinions 🦋💖"
+  }
+];
+
+//first we need to show all the users
+//second - filter karna har baar input karne par
+//third- show karna filtered users
