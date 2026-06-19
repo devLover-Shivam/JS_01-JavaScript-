07e2215ffcb3e8c683ac7b5d52c1bdb9c42cb5e6
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-06-19 13:49:03 +0530

    complete user finding project

--- project_01.js
+++ project_01.js
@@ -21,12 +21,12 @@ const users = [
   },
   {
     name: "Ananya Mehra",
-    pic: "https://i.pinimg.com/736x/ba/9f/e8/ba9fe8f87ead2a7260de8e50d7afcd53.jpg",
+    pic: "https://i.pinimg.com/736x/f1/12/4a/f1124a3c656d256a7b288061a8a5da71.jpg",
     bio: "born to stand out, never to fit in ✨😌"
   },
   {
     name: "Tanya Arora",
-    pic: "https://i.pinimg.com/736x/70/01/5d/70015d367bf637437379d5db34b37203.jpg",
+    pic: "https://i.pinimg.com/736x/fa/11/6e/fa116e6f7c90ae6b033a3b812a1ed5ce.jpg",
     bio: "expensive taste, free advice 💸😉"
   },
   {
@@ -37,5 +37,63 @@ const users = [
 ];
 
 //first we need to show all the users
+//showUsers function me arr pass karenge aur har ek user k liye ek naya function chalege through forEach.
+function showUsers(arr){
+    arr.forEach(function(user) {
+        // Main container
+        const cardContainer = document.querySelector(".flex.gap-10");
+
+        // Card
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // Image
+        const img = document.createElement("img");
+        img.src =
+        user.pic;
+        img.classList.add("bg-img");
+
+        // Blurred Layer
+        const blurredLayer = document.createElement("div");
+        blurredLayer.style.backgroundImage = `url(${user.pic})`;
+        blurredLayer.classList.add("blurred-layer");
+
+        // Content
+        const content = document.createElement("div");
+        content.classList.add("content");
+
+        // Heading
+        const heading = document.createElement("h3");
+        heading.textContent = user.name;
+
+        // Paragraph
+        const para = document.createElement("p");
+        para.textContent = user.bio;
+
+        // Build Structure
+        content.appendChild(heading);
+        content.appendChild(para);
+
+        card.appendChild(img);
+        card.appendChild(blurredLayer);
+        card.appendChild(content);
+       
+       document.querySelector(".cards").appendChild(card) ;
+    });
+}
+showUsers(users);
 //second - filter karna har baar input karne par
+let inp = document.querySelector(".inp");
+inp.addEventListener("input",function(){
+    /* filter actually me andar jo bhi element match kar jaaega input value se usme true return kar dega aur wo value fir pass ho jaaega newUsers variable me */
+    let newUsers = users.filter((user) => {
+        return user.name.toLowerCase().includes(inp.value.toLowerCase());
+    });
+    console.log(newUsers);
+    document.querySelector(".cards").innerHTML = "";
+    showUsers(newUsers);
+})
+
 //third- show karna filtered users
+
+//new changes :- no users found message if the user is not found, and highlighted search text functionality
