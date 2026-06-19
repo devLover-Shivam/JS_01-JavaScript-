dcb057c155d4616b252dc6d2d26aff15cdc8413d
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-06-19 14:28:38 +0530

    Add about scopes in JS

---
+++ scope.js
@@ -0,0 +1,7 @@
+//scope - functional scope, global scope and block scope
+//function scope - function k andar hi use ho skti hai
+//global scope- poore code me kahi bhi use ho skti hai
+//block scope- {curly braces me hi use ho sakti hai}
+
+//agar aapka code kisi bhi {} k andar nhi hai to aapka code global hai !
+
